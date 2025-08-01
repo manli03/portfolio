708102b341e2f460680ee76751c4aa0b7fc2aadb
aiman <aimanmuhd993@gmail.com>
2025-08-02 03:45:58 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#111111',
+        accent: '#1DCD9F',
+        soft: '#222222',
+        secondary: '#169976',
+      },
+    },
+  },
+  plugins: [],
+};
